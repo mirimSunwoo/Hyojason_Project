cd587b25b121538952f4e3255f72694b08158530
Leesunwoo <73941301+mirimSunwoo@users.noreply.github.com>
2021-07-26 15:50:57 +0900

    Initial commit

---
+++ app/src/main/java/kr/hs/emirim/sunw2311/hyojason_project/MainActivity.java
@@ -0,0 +1,14 @@
+package kr.hs.emirim.sunw2311.hyojason_project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
