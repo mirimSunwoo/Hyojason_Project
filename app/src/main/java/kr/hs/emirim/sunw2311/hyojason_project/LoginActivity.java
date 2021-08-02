ddc82bdb1b16330fbd8c98c36788fd41d587a821
Leesunwoo <73941301+mirimSunwoo@users.noreply.github.com>
2021-08-02 23:18:33 +0900

    Edit SecondActivity.java Change LoginActivity.java Commit

--- app/src/main/java/kr/hs/emirim/sunw2311/hyojason_project/LoginActivity.java
+++ app/src/main/java/kr/hs/emirim/sunw2311/hyojason_project/LoginActivity.java
@@ -1,14 +1,12 @@
 package kr.hs.emirim.sunw2311.hyojason_project;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
-
+public class LoginActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_recipe_food);
+        setContentView(R.layout.activity_login);
     }
 }
