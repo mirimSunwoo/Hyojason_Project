e8d3c221c8d62c2637d99fc73cc08570170a2a4a
효정이 <w2029@e-mirim.hs.kr>
2021-08-02 23:02:46 +0900

    MainActivity.java and SecondActivity.java(Login) Commit

--- app/src/main/java/kr/hs/emirim/sunw2311/hyojason_project/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/sunw2311/hyojason_project/MainActivity.java
@@ -2,13 +2,29 @@ package kr.hs.emirim.sunw2311.hyojason_project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button btnStart;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_recipe_food);
+        setContentView(R.layout.activity_main);
+
+        btnStart = findViewById(R.id.btn_start);
+        btnStart.setOnClickListener(btnListener);
+
     }
+    View.OnClickListener btnListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Intent intent = new Intent(getApplicationContext(), SecondActivity.class);
+            startActivity(intent);
+            finish();
+        }
+    };
 }
