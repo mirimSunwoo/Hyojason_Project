feaabd4debbeefff100ed3d75dc73e15b8b4dbb5
효정이 <w2029@e-mirim.hs.kr>
2021-08-03 15:17:13 +0900

    Join_Connection(change) Commit

---
+++ app/src/main/java/kr/hs/emirim/sunw2311/hyojason_project/JoinActivity.java
@@ -0,0 +1,35 @@
+package kr.hs.emirim.sunw2311.hyojason_project;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import org.w3c.dom.Text;
+
+public class JoinActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_join);
+
+        ActionBar abar = getSupportActionBar();
+        abar.hide();
+
+        TextView textJoin;
+
+        textJoin = findViewById(R.id.text_join);
+        textJoin.setOnClickListener(btnListener);
+    }
+    View.OnClickListener btnListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+            startActivity(intent);
+            finish();
+        }
+    };
+}
