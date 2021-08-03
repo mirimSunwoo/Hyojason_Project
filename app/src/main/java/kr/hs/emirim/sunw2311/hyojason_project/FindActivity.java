8ece8b11bd1529fc6bf031782b74716e1a7daeed
효정이 <w2029@e-mirim.hs.kr>
2021-08-03 15:16:50 +0900

    Find_id_password_Connection(change) Commit

---
+++ app/src/main/java/kr/hs/emirim/sunw2311/hyojason_project/FindActivity.java
@@ -0,0 +1,47 @@
+package kr.hs.emirim.sunw2311.hyojason_project;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import org.w3c.dom.Text;
+
+public class FindActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_find_id_pass);
+
+        ActionBar abar = getSupportActionBar();
+        abar.hide();
+
+        TextView textCheck1, textCheck2;
+
+        textCheck1 = findViewById(R.id.text_check1);
+        textCheck2 = findViewById(R.id.text_check2);
+
+        textCheck1.setOnClickListener(btnListener);
+        textCheck2.setOnClickListener(btnListener1);
+
+    }
+    View.OnClickListener btnListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+            startActivity(intent);
+            finish();
+        }
+    };
+    View.OnClickListener btnListener1 = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+            startActivity(intent);
+            finish();
+        }
+    };
+}
