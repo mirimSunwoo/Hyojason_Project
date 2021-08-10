83ac60bdf449e8180ed0e6a7e67efb6f92bf093d
Leesunwoo <73941301+mirimSunwoo@users.noreply.github.com>
2021-08-11 00:11:46 +0900

    Edit SearchActivity Commit(이틀연속 회의 ing...)

--- app/src/main/java/kr/hs/emirim/sunw2311/hyojason_project/MenuActivity.java
+++ app/src/main/java/kr/hs/emirim/sunw2311/hyojason_project/MenuActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.media.Image;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageButton;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,16 +13,18 @@ public class MenuActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         ImageButton btnMyPage, btnBest, btnReview, btnRecipe, btnTimer;
+        EditText btnSearch;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
 
-
+        btnSearch = findViewById(R.id.btn_menu_search);
         btnMyPage = findViewById(R.id.btn_mypage);
         btnBest = findViewById(R.id.btn_menu_best);
         btnReview = findViewById(R.id.btn_menu_review);
         btnRecipe = findViewById(R.id.btn_menu_recipe);
         btnTimer = findViewById(R.id.btn_menu_timer);
 
+        btnSearch.setOnClickListener(btnSearchListener);
         btnMyPage.setOnClickListener(btnListener);
         btnBest.setOnClickListener(btnBestListener);
         btnReview.setOnClickListener(btnReviewListener);
@@ -29,6 +32,14 @@ public class MenuActivity extends AppCompatActivity {
         btnTimer.setOnClickListener(btnTimerListener);
 
     }
+    View.OnClickListener btnSearchListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Intent intent = new Intent(getApplicationContext(), SearchActivity.class);
+            startActivity(intent);
+            finish();
+        }
+    };
     ImageButton.OnClickListener btnListener = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
