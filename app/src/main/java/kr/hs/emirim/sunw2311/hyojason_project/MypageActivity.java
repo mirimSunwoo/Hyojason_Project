b3a923e0bbb11ac7d1123789796421843a138736
Leesunwoo <73941301+mirimSunwoo@users.noreply.github.com>
2021-08-10 22:07:52 +0900

    MypageActivity.java and MypageSettingActivity.java

---
+++ app/src/main/java/kr/hs/emirim/sunw2311/hyojason_project/MypageActivity.java
@@ -0,0 +1,15 @@
+package kr.hs.emirim.sunw2311.hyojason_project;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MypageActivity extends AppCompatActivity {
+    Button MyPagebtn;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mypage);
+    }
+}
