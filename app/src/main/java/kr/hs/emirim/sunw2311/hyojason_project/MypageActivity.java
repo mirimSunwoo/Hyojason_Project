2a562d871aad9a2907779cd08c0fef1eb6a7e4cd
Leesunwoo <73941301+mirimSunwoo@users.noreply.github.com>
2021-08-11 00:20:36 +0900

    Edit MypageActivity.java & activity_mypage_setting.xml(Edit Back button)Commit

--- app/src/main/java/kr/hs/emirim/sunw2311/hyojason_project/MypageActivity.java
+++ app/src/main/java/kr/hs/emirim/sunw2311/hyojason_project/MypageActivity.java
@@ -1,15 +1,40 @@
 package kr.hs.emirim.sunw2311.hyojason_project;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MypageActivity extends AppCompatActivity {
-    Button MyPagebtn;
+    ImageButton btnHome, btnSetting;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mypage);
+
+        btnHome = findViewById(R.id.btn_mypage_home);
+        btnHome.setOnClickListener(btnHomeListener);
+
+        btnSetting = findViewById(R.id.btn_setting);
+        btnSetting.setOnClickListener(btnSettingListener);
     }
+    View.OnClickListener btnHomeListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Intent intent = new Intent(getApplicationContext(), MenuActivity.class);
+            startActivity(intent);
+            finish();
+        }
+    };
+    View.OnClickListener btnSettingListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Intent intent = new Intent(getApplicationContext(), MypageSettingActivity.class);
+            startActivity(intent);
+            finish();
+        }
+    };
 }
