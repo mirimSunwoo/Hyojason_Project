6b84379cae1aac1a4702541b1565891dabb423c2
Leesunwoo <73941301+mirimSunwoo@users.noreply.github.com>
2021-08-12 00:43:02 +0900

    Edit JoinActivity.java ing....

--- app/src/main/java/kr/hs/emirim/sunw2311/hyojason_project/JoinActivity.java
+++ app/src/main/java/kr/hs/emirim/sunw2311/hyojason_project/JoinActivity.java
@@ -18,26 +18,27 @@ import org.w3c.dom.Text;
 
 public class JoinActivity extends AppCompatActivity {
     MyDBHelper dbHelper;
-    EditText joinId, joinPassword, joinPetName, joinEmail;
+    EditText joinId, joinPassword, joinPetName, joinEmail, editNamesResult, editIdresult;
     TextView listCheck, btnJoin;
-    SQLiteDatabase db;
+    SQLiteDatabase db ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_join);
-
-        ActionBar abar = getSupportActionBar();
-        abar.hide();
+        setTitle("회원가입 DB");
+//        ActionBar abar = getSupportActionBar();
+//        abar.hide();
         joinEmail = findViewById(R.id.join_email);
         joinId = findViewById(R.id.join_id);
         joinPassword = findViewById(R.id.join_password);
         joinPetName = findViewById(R.id.join_petName);
-
         btnJoin = findViewById(R.id.btn_join);
 
-        listCheck = findViewById(R.id.list_check);
-        listCheck.setOnClickListener(listListener);
+        editNamesResult = findViewById(R.id.edtNameResult);
+        editIdresult = findViewById(R.id.edtIdResult);
+//        listCheck = findViewById(R.id.list_check);
+//        listCheck.setOnClickListener(listListener);
 
         dbHelper = new MyDBHelper(this);
 
@@ -45,7 +46,12 @@ public class JoinActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 db = dbHelper.getWritableDatabase();
-                db.execSQL("insert into groupTB values('"+joinPetName.getText().toString()+"',"+joinId.getText().toString()+"',"+joinPassword.getText().toString()+"',"+joinEmail+");");
+                db.execSQL("INSERT INTO groupTB VALUES (" +
+                        "'" + joinPetName.getText().toString() + "' ,"
+                        + joinId.getText().toString() + "' ,"
+                        + joinPassword.getText().toString() + "' ,"
+                        + joinEmail.getText().toString() + ");");
+                db.close();
                 Toast.makeText(getApplicationContext(),"정상적으로 행이 삽입 되었습니다.",Toast.LENGTH_SHORT).show();
                 selectDB();
                 Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
@@ -54,14 +60,14 @@ public class JoinActivity extends AppCompatActivity {
             }
         });
     }
-    View.OnClickListener listListener = new View.OnClickListener() {
-        @Override
-        public void onClick(View v) {
-            Intent intent = new Intent(getApplicationContext(), ListViewActivity.class);
-            startActivity(intent);
-            finish();
-        }
-    };
+    //    View.OnClickListener listListener = new View.OnClickListener() {
+//        @Override
+//        public void onClick(View v) {
+//            Intent intent = new Intent(getApplicationContext(), ListViewActivity.class);
+//            startActivity(intent);
+//            finish();
+//        }
+//    };
     public void selectDB(){
         db = dbHelper.getReadableDatabase();
         Cursor cursor = db.rawQuery("select * from groupTB;", null);
@@ -75,10 +81,12 @@ public class JoinActivity extends AppCompatActivity {
             password+=cursor.getInt(2) + "\r\n";
             email+=cursor.getInt(2) + "\r\n";
         }
-        joinPetName.setText(petName);
-        joinId.setText(Id);
-        joinPassword.setText(password);
-        joinEmail.setText(email);
+//        joinPetName.setText(petName);
+//        joinId.setText(Id);
+//        joinPassword.setText(password);
+//        joinEmail.setText(email);
+        editNamesResult.setText(petName);
+        editIdresult.setText(Id);
 
         cursor.close();
         db.close();
@@ -86,11 +94,11 @@ public class JoinActivity extends AppCompatActivity {
     public class MyDBHelper extends SQLiteOpenHelper {
 
         public MyDBHelper(Context context){
-            super(context,"groupDB",null, 1);
+            super(context,"groupTB",null, 1);
         }
         @Override
         public void onCreate(SQLiteDatabase db) {
-            db.execSQL("create table groupTB(name char(20) primary key, id char, password char, email char);");
+            db.execSQL("create table groupTB(petName char(20) primary key, id char, password char, email char);");
         }
 
         @Override
