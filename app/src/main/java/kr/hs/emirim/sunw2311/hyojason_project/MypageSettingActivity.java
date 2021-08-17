0d19d6476eb414afffb6d9f2d44faa0eea93e454
Leesunwoo <73941301+mirimSunwoo@users.noreply.github.com>
2021-08-17 23:18:40 +0900

    Edit ReviewActivity.java, SearchActivity.java, TimerActivity.java HomeListener Commit

--- app/src/main/java/kr/hs/emirim/sunw2311/hyojason_project/MypageSettingActivity.java
+++ app/src/main/java/kr/hs/emirim/sunw2311/hyojason_project/MypageSettingActivity.java
@@ -1,15 +1,28 @@
 package kr.hs.emirim.sunw2311.hyojason_project;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.widget.Button;
+import android.view.View;
+import android.widget.ImageButton;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MypageSettingActivity extends AppCompatActivity {
-    Button MyPagebtn;
+    ImageButton btnBack;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_mypage_setting);
+        setContentView(R.layout.activity_recipe_food_ex);
+
+        btnBack= findViewById(R.id.btn_setting_back);
+        btnBack.setOnClickListener(btnBackListener);
     }
+    View.OnClickListener btnBackListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Intent intent = new Intent(getApplicationContext(), MypageActivity.class);
+            startActivity(intent);
+            finish();
+        }
+    };
 }
