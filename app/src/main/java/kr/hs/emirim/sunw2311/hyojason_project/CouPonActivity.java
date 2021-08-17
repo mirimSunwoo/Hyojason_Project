f63cf80e35ee8a363e2fd9187315cdaecad4a9c6
Leesunwoo <73941301+mirimSunwoo@users.noreply.github.com>
2021-08-18 00:01:17 +0900

    please be successful

--- app/src/main/java/kr/hs/emirim/sunw2311/hyojason_project/CouPonActivity.java
+++ app/src/main/java/kr/hs/emirim/sunw2311/hyojason_project/CouPonActivity.java
@@ -1,8 +1,4 @@
 package kr.hs.emirim.sunw2311.hyojason_project;
-
-<<<<<<< HEAD
-public class CouPonActivity {
-=======
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,5 +9,4 @@ public class CouPonActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_coupon);
     }
->>>>>>> bc0e1c59dce6772f4a760f3fc7c53e44530a9b04
 }
