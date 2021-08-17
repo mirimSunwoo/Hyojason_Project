c5ae55156f996d8cf49b1a528b9574cf40169dc1
효정이 <w2029@e-mirim.hs.kr>
2021-08-18 00:13:24 +0900

    CouPonActivity (incomplete2) Commit

--- app/src/main/java/kr/hs/emirim/sunw2311/hyojason_project/CouPonActivity.java
+++ app/src/main/java/kr/hs/emirim/sunw2311/hyojason_project/CouPonActivity.java
@@ -1,13 +1,30 @@
 package kr.hs.emirim.sunw2311.hyojason_project;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class CouPonActivity extends AppCompatActivity {
+    TextView coubtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_coupon);
+
+        coubtn = findViewById(R.id.btn_coupon);
+
+        coubtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), CouPonScreenActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+
     }
 }
---
+++ app/src/main/java/kr/hs/emirim/sunw2311/hyojason_project/CouPonScreenActivity.java
@@ -0,0 +1,13 @@
+package kr.hs.emirim.sunw2311.hyojason_project;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class CouPonScreenActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_coupon_screen);
+    }
+}
