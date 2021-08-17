6102ce35987b4c9819ea1b06095271e948d27c5f
Leesunwoo <73941301+mirimSunwoo@users.noreply.github.com>
2021-08-18 00:12:18 +0900

    Edit "로그아웃 하시겠습니까?" toast Commit

--- app/src/main/java/kr/hs/emirim/sunw2311/hyojason_project/MypageActivity.java
+++ app/src/main/java/kr/hs/emirim/sunw2311/hyojason_project/MypageActivity.java
@@ -1,11 +1,13 @@
 package kr.hs.emirim.sunw2311.hyojason_project;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MypageActivity extends AppCompatActivity {
@@ -23,7 +25,32 @@ public class MypageActivity extends AppCompatActivity {
         btnSetting.setOnClickListener(btnSettingListener);
 
         btnLogout = findViewById(R.id.btn_mypage_logout);
-        btnLogout.setOnClickListener(btnLogoutListener);
+        btnLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(MypageActivity.this);
+                builder.setMessage("로그아웃 하시겠습니까?");
+                builder.setTitle("로그아웃 알림창")
+                        .setCancelable(false)
+                        .setPositiveButton("네", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+                                startActivity(intent);
+                                finish();
+                            }
+                        })
+                        .setNegativeButton("아니오", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                dialog.cancel();
+                            }
+                        });
+                AlertDialog alert = builder.create();
+                alert.setTitle("로그아웃");
+                alert.show();
+            }
+        });
     }
     View.OnClickListener btnHomeListener = new View.OnClickListener() {
         @Override
