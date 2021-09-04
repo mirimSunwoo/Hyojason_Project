aab99d06a2a6b5baa5b096da3654c54b3d9d30ff
Leesunwoo <73941301+mirimSunwoo@users.noreply.github.com>
2021-09-04 22:39:12 +0900

    activity_find_id_pass.xml 레이아웃 조정 activity_join.xml 버튼위치 조정 back버튼 추가

--- app/src/main/java/kr/hs/emirim/sunw2311/hyojason_project/FindActivity.java
+++ app/src/main/java/kr/hs/emirim/sunw2311/hyojason_project/FindActivity.java
@@ -3,6 +3,7 @@ package kr.hs.emirim.sunw2311.hyojason_project;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 import androidx.appcompat.app.ActionBar;
@@ -20,15 +21,26 @@ public class FindActivity extends AppCompatActivity {
         abar.hide();
 
         TextView textCheck1, textCheck2;
+        ImageButton BackButton;
 
         textCheck1 = findViewById(R.id.text_check1);
         textCheck2 = findViewById(R.id.text_check2);
+        BackButton = findViewById(R.id.btn_find_back);
 
         textCheck1.setOnClickListener(btnListener);
         textCheck2.setOnClickListener(btnListener);
+        BackButton.setOnClickListener(btnBackListener);
 
     }
     View.OnClickListener btnListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+            startActivity(intent);
+            finish();
+        }
+    };
+    View.OnClickListener btnBackListener = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
             Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
