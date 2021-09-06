1cf5d01acd50ff7df975fdd093574179498d78d9
Leesunwoo <73941301+mirimSunwoo@users.noreply.github.com>
2021-09-06 19:38:31 +0900

    activity_mypage_manager.xml Commit

--- app/src/main/java/kr/hs/emirim/sunw2311/hyojason_project/ManagerActivity.java
+++ app/src/main/java/kr/hs/emirim/sunw2311/hyojason_project/ManagerActivity.java
@@ -1,4 +1,28 @@
 package kr.hs.emirim.sunw2311.hyojason_project;
 
-public class ManagerActivity {
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ManagerActivity extends AppCompatActivity {
+    ImageButton btnBack;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mypage_manager);
+
+        btnBack = findViewById(R.id.btn_recipe_back);
+        btnBack.setOnClickListener(btnBackListener);
+    }
+    View.OnClickListener btnBackListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Intent intent = new Intent(getApplicationContext(), RecipeActivity.class);
+            startActivity(intent);
+            finish();
+        }
+    };
 }
