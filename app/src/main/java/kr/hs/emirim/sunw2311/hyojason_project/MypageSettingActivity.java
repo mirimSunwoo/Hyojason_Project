5f6e77d991a32a203d5e9bc59a75af3d60899ab3
효정이 <w2029@e-mirim.hs.kr>
2021-09-09 19:56:21 +0900

    MypageSettingActivity.java (img change btn) Commit

--- app/src/main/java/kr/hs/emirim/sunw2311/hyojason_project/MypageSettingActivity.java
+++ app/src/main/java/kr/hs/emirim/sunw2311/hyojason_project/MypageSettingActivity.java
@@ -2,19 +2,23 @@ package kr.hs.emirim.sunw2311.hyojason_project;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import java.text.BreakIterator;
 
 public class MypageSettingActivity extends AppCompatActivity {
     ImageButton btnBack;
-    ImageView onImg;
+    ImageView changeBtn;
     private TextView tv;
 
     @Override
@@ -22,12 +26,13 @@ public class MypageSettingActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mypage_setting);
 
-        btnBack= findViewById(R.id.btn_setting_back);
-        onImg = findViewById(R.id.change_img);
-        btnBack.setOnClickListener(btnBackListener);
+        btnBack = findViewById(R.id.btn_setting_back);
+        changeBtn = findViewById(R.id.change_img);
+        registerForContextMenu(changeBtn);
 
 
     }
+
     View.OnClickListener btnBackListener = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
@@ -36,4 +41,21 @@ public class MypageSettingActivity extends AppCompatActivity {
             finish();
         }
     };
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        super.onCreateContextMenu(menu, v, menuInfo);
+    }
+
+    @Override
+    public boolean onContextItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menu1:
+                Toast.makeText(getApplicationContext(), "사진변경", Toast.LENGTH_SHORT).show();
+            return true;
+        }
+        return false;
+    }
+
 }
