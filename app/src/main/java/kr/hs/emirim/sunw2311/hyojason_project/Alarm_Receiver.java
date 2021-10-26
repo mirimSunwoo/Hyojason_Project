a717d508090670fd0c301580be6a0033004fd553
효정이 <w2029@e-mirim.hs.kr>
2021-10-26 20:14:18 +0900

    알람 많이 완성(소리 못 울림)

--- app/src/main/java/kr/hs/emirim/sunw2311/hyojason_project/Alarm_Receiver.java
+++ app/src/main/java/kr/hs/emirim/sunw2311/hyojason_project/Alarm_Receiver.java
@@ -1,32 +1,50 @@
 package kr.hs.emirim.sunw2311.hyojason_project;
 
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.TaskStackBuilder;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.media.RingtoneManager;
 import android.os.Build;
+
+import androidx.core.app.NotificationCompat;
 
 public class Alarm_Receiver extends BroadcastReceiver {
     Context context;
+    String channelId = "alarm_channel";
 
     @Override
     public void onReceive(Context context, Intent intent) {
 
         this.context = context;
-        // intent로부터 전달받은 string
-        String get_your_string = intent.getExtras().getString("state");
+       Intent busRouteIntent = new Intent(context, TimerSettingActivity.class);
 
-        //RingtonePlayingService 서비스 intent 생성
-        Intent service_intent = new Intent(context, RingtonePlayingService.class);
+        TaskStackBuilder stackBuilder = TaskStackBuilder.create(context);
+        stackBuilder.addNextIntentWithParentStack(busRouteIntent);
+        PendingIntent busRoutePendingIntent = stackBuilder.getPendingIntent(1, PendingIntent.FLAG_UPDATE_CURRENT);
 
-        //RingtonePlayService로 extra string값 보내기
-        service_intent.putExtra("state", get_your_string);
-        //start the ringtone service
+        final NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(context, channelId)
+                .setSmallIcon(R.mipmap.ic_launcher).setDefaults(Notification.DEFAULT_ALL)
+                .setSound(RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION))
+                .setContentTitle("알람")
+                .setContentText("울림")
+                .setContentIntent(busRoutePendingIntent);
 
-        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O){
-            this.context.startForegroundService(service_intent);
-        }else{
-            this.context.startService(service_intent);
+        final NotificationManager notificationManager = (NotificationManager)context.getSystemService(Context.NOTIFICATION_SERVICE);
+
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+            NotificationChannel channel = new NotificationChannel(channelId, "Channel human readable", NotificationManager.IMPORTANCE_DEFAULT);
+            notificationManager.createNotificationChannel(channel);
         }
+        int id = (int)System.currentTimeMillis();
+
+        notificationManager.notify(id, notificationBuilder.build());
+
+
     }
 }
 
--- app/src/main/java/kr/hs/emirim/sunw2311/hyojason_project/TimerSettingActivity.java
+++ app/src/main/java/kr/hs/emirim/sunw2311/hyojason_project/TimerSettingActivity.java
@@ -9,6 +9,7 @@ import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.os.Build;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
@@ -27,64 +28,42 @@ import java.util.Locale;
 
 public class TimerSettingActivity extends AppCompatActivity {
     TimePicker timePicker;
-    Context context;
-    PendingIntent pendingIntent;
-    AlarmManager alarmManager;
-    ImageButton btn_back;
+    Button check_btn;
+    Intent intent2;
 
+    @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_timer_setting);
 
-        btn_back = findViewById(R.id.timer_setting__back);
-        btn_back.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), TimerActivity.class);
-                startActivity(intent);
+        timePicker = findViewById(R.id.tp_timepicker);
+        check_btn = findViewById(R.id.btn_Check);
+
+        check_btn.setOnClickListener(v -> {
+            Calendar calendar = Calendar.getInstance();
+            calendar.setTimeInMillis(System.currentTimeMillis());
+            int hour = timePicker.getHour();
+            int minute = timePicker.getMinute();
+            calendar.get(Calendar.HOUR_OF_DAY);
+            calendar.set(Calendar.MINUTE, minute);
+
+            if (calendar.before(Calendar.getInstance())){
+                calendar.add(Calendar.DATE, 1);
+            }
+            AlarmManager alarmManager = (AlarmManager)this.getSystemService(Context.ALARM_SERVICE);
+            if(alarmManager != null){
+                Intent intent = new Intent(this, Alarm_Receiver.class);
+                PendingIntent alarmIntent = PendingIntent.getBroadcast(this, 1, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+                alarmManager.setRepeating(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), AlarmManager.INTERVAL_DAY, alarmIntent);
+
+                Intent intent2 = new Intent(getApplicationContext(), TimerActivity.class);
+                startActivity(intent2);
                 finish();
+
+                Toast.makeText(TimerSettingActivity.this,"알람이 저장되었습니다.", Toast.LENGTH_SHORT).show();
             }
         });
-
-        this.context = this;
-
-        //알람매니저 설정
-        alarmManager = (AlarmManager) getSystemService(ALARM_SERVICE);
-
-        //타임피커 설정
-        timePicker = findViewById(R.id.tp_timepicker);
-
-        // Calendar 객체 생성
-        final Calendar calendar = Calendar.getInstance();
-
-        //알람리시버 intent 생성
-        final Intent my_intent = new Intent(this.context, Alarm_Receiver.class);
-
-        // 알람 시작 버튼
-        Button alarm_on = findViewById(R.id.btn_Check);
-        alarm_on.setOnClickListener(new View.OnClickListener() {
-                @RequiresApi(api = Build.VERSION_CODES.M)
-                @Override
-                public void onClick(View v) {
-                    // calendar에 시간 셋팅
-                    calendar.set(Calendar.HOUR_OF_DAY, timePicker.getHour());
-                    calendar.set(Calendar.MINUTE, timePicker.getMinute());
-
-                    //시간 가져옴
-                    int hour = timePicker.getHour();
-                    int minute = timePicker.getMinute();
-                    Toast.makeText(TimerSettingActivity.this,"hour + 시 " + minute + "분" + " 알람 예정", Toast.LENGTH_SHORT);
-
-                    // reveiver에 string값 넘겨주기
-                    my_intent.putExtra("state", "alarm on");
-
-                    pendingIntent = PendingIntent.getBroadcast(TimerSettingActivity.this, 0, my_intent, PendingIntent.FLAG_UPDATE_CURRENT);
-
-                    //알람 셋팅
-                    alarmManager.set(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), pendingIntent);
-                }
-        });
-
     }
 }
