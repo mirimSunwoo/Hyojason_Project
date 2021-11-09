65e6a3e9dfc33aa8250a43d5215b266b6efc1f06
효정이 <w2029@e-mirim.hs.kr>
2021-11-09 19:51:21 +0900

    알람 소리 울리기 성공!!

--- app/src/main/java/kr/hs/emirim/sunw2311/hyojason_project/Alarm_Receiver.java
+++ app/src/main/java/kr/hs/emirim/sunw2311/hyojason_project/Alarm_Receiver.java
@@ -15,12 +15,29 @@ import androidx.core.app.NotificationCompat;
 
 public class Alarm_Receiver extends BroadcastReceiver {
     Context context;
-    String channelId = "alarm_channel";
+//    String channelId = "alarm_channel";
 
     @Override
     public void onReceive(Context context, Intent intent) {
-
         this.context = context;
+        // intent로부터 전달받은 string
+        String get_yout_string = intent.getExtras().getString("state");
+
+        // RingtonePlayingService 서비스 intent 생성
+        Intent service_intent = new Intent(context, RingtonePlayingService.class);
+
+        // RingtonePlayinService로 extra string값 보내기
+        service_intent.putExtra("state", get_yout_string);
+        // start the ringtone service
+
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O){
+            this.context.startForegroundService(service_intent);
+        }else{
+            this.context.startService(service_intent);
+        }
+
+
+/*        this.context = context;
        Intent busRouteIntent = new Intent(context, TimerSettingActivity.class);
 
         TaskStackBuilder stackBuilder = TaskStackBuilder.create(context);
@@ -42,7 +59,7 @@ public class Alarm_Receiver extends BroadcastReceiver {
         }
         int id = (int)System.currentTimeMillis();
 
-        notificationManager.notify(id, notificationBuilder.build());
+        notificationManager.notify(id, notificationBuilder.build());*/
 
 
     }
