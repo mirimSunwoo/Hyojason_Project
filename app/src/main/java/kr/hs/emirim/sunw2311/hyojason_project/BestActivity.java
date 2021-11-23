b48e40a3ad51c907e1e87271333136faba84c40e
Leesunwoo <73941301+mirimSunwoo@users.noreply.github.com>
2021-11-24 00:29:10 +0900

    Best Food BackButton 활성화

--- app/src/main/java/kr/hs/emirim/sunw2311/hyojason_project/BestActivity.java
+++ app/src/main/java/kr/hs/emirim/sunw2311/hyojason_project/BestActivity.java
@@ -2,11 +2,16 @@ package kr.hs.emirim.sunw2311.hyojason_project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
 import android.widget.Gallery;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 
 public class BestActivity extends AppCompatActivity {
+    ImageButton btnBack;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,5 +22,17 @@ public class BestActivity extends AppCompatActivity {
         Gallery gallery = findViewById(R.id.gallery1);
         GalleryAdapter adapter = new GalleryAdapter(this, imgvLarge);
         gallery.setAdapter(adapter);
+
+        btnBack = findViewById(R.id.btn_best_back);
+
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), MenuActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
+
 }
