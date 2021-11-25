6b7dc8c7b40b90037c62249ba334edfdb3e27c3c
효정이 <w2029@e-mirim.hs.kr>
2021-11-25 18:54:46 +0900

    자동 로그인 완성!!

--- app/src/main/java/kr/hs/emirim/sunw2311/hyojason_project/LoginActivity.java
+++ app/src/main/java/kr/hs/emirim/sunw2311/hyojason_project/LoginActivity.java
@@ -6,15 +6,20 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class LoginActivity extends AppCompatActivity {
     int version = 3;
@@ -28,18 +33,38 @@ public class LoginActivity extends AppCompatActivity {
     String sql;
     Cursor cursor;
 
+    CheckBox checkBox;
+
+    SharedPreferences setting;
+    SharedPreferences.Editor editor;
+
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+
         idEdit = findViewById(R.id.id_input);
         passEdit = findViewById(R.id.pass_input);
 
-        textFind = findViewById(R.id.text_find);
+//        textFind = findViewById(R.id.text_find);
 
         textLogin = findViewById(R.id.text_login);
         btnJoin = findViewById(R.id.btn_join);
+
+        checkBox = findViewById(R.id.check_box);
+
+        setting = getSharedPreferences("setting", 0);
+        editor=setting.edit();
+
+        if(setting.getBoolean("chk_auto", false)){
+            idEdit.setText(setting.getString("ID",""));
+            passEdit.setText(setting.getString("PW", ""));
+            checkBox.setChecked(true);
+        }
 
         helper = new DatabaseOpenHelper(LoginActivity.this, DatabaseOpenHelper.tableName, null, version);
         database = helper.getWritableDatabase();
@@ -47,6 +72,21 @@ public class LoginActivity extends AppCompatActivity {
         textLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                if(checkBox.isChecked()){
+                    Toast.makeText(LoginActivity.this, "자동 로그인", Toast.LENGTH_SHORT).show();
+
+                    String ID = idEdit.getText().toString();
+                    String PW = passEdit.getText().toString();
+
+                    editor.putString("ID", ID);
+                    editor.putString("PW", PW);
+                    editor.putBoolean("chk_auto", true);
+                    editor.commit();
+
+                }else{
+                    editor.clear();
+                    editor.commit();
+                }
                 String id = idEdit.getText().toString();
                 String pw = passEdit.getText().toString();
 
@@ -96,13 +136,13 @@ public class LoginActivity extends AppCompatActivity {
                 finish();
             }
         });
-        textFind.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), FindActivity.class);
-                startActivity(intent);
-                finish();
-            }
-        });
+//        textFind.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent intent = new Intent(getApplicationContext(), FindActivity.class);
+//                startActivity(intent);
+//                finish();
+//            }
+//        });
     }
 }
